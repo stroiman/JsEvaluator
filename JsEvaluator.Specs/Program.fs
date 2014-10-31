7f7659e457a998ade1b9743f2be924b5a8087601
Peter Strøiman <peter@stroiman.com>
2014-10-31 16:08:18 +0100

    Added skeleton FSpec code

--- JsEvaluator.Specs/Program.fs
+++ JsEvaluator.Specs/Program.fs
@@ -1,7 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+[<MbUnit.Framework.TestFixture>]
+type Wrapper() =
+    inherit FSpec.MbUnitWrapper.MbUnitWrapperBase()
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    System.Reflection.Assembly.GetExecutingAssembly ()
+    |> FSpec.TestDiscovery.runSingleAssembly
