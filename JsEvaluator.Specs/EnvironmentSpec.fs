f24c7a7a7e183a49de765b8a2ce06a814a97b431
Peter Strøiman <peter@stroiman.com>
2014-10-31 22:19:35 +0100

    Enabled setting and getting single value

---
+++ JsEvaluator.Specs/EnvironmentSpec.fs
@@ -0,0 +1,31 @@
+﻿module EnvironmentSpec
+open FSpec
+open FSpec.Dsl
+open FSpec.Matchers
+open Runtime
+
+type TestContext
+  with
+    member self.Environment 
+      with get () : Environment = self?environment
+      and set (value:Environment) = self?environment <- value
+
+let specs = 
+  describe "Environment" [
+    before (fun ctx -> ctx.Environment <- Environment.Create ())
+
+    describe ".getVariable()" [
+      context "when variable doesn't exist" [
+        it "returns 'undefined'" (fun c ->
+          c.Environment.Get "foo" |> should (be.equalTo JsUndefined)
+        )
+      ]
+
+      context "when variable exists" [
+        before (fun x -> x.Environment.Add "key" (JsString "Foo"))
+
+        it "returns the value" (fun c ->
+          c.Environment.Get "key" |> should (be.equalTo (JsString "Foo")))
+      ]
+    ]
+  ]
---
+++ JsEvaluator/Runtime.fs
@@ -0,0 +1,17 @@
+﻿module Runtime
+
+type JsVariableType =
+  | JsUndefined
+  | JsString of string
+
+type Environment = {
+    mutable data : Map<string,JsVariableType>
+  }
+  with
+    member self.Add name value = 
+        self.data <- self.data.Add (name,value)
+    member self.Get name = 
+        match self.data.TryFind name with
+        | Some x -> x
+        | None -> JsUndefined
+    static member Create () = { data = Map.empty }
