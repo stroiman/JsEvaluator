732afc7e1523d3523335bcb1b9303adab20179ae
Peter Strøiman <peter@stroiman.com>
2014-11-02 16:39:46 +0100

    Reduced duplication with helper methods

--- JsEvaluator.Specs/ParserSpecs.fs
+++ JsEvaluator.Specs/ParserSpecs.fs
@@ -4,18 +4,28 @@ open FSpec.Dsl
 open FSpec.Matchers
 open Ast
 
+let parseSingleStatement =
+  JsEval.parse >> function
+  | Program[x] -> x
+  | _ -> failwith "expected program with one statement"
+
+let parseSingleExpression =
+  parseSingleStatement >> function
+  | ExpressionStmt x -> x
+  | _ -> failwith "expected an expression"
+
 let specs =
   describe "Parse" [
     it "Parses a single number" (fun _ ->
-      let expected = Program [ ExpressionStmt ( NumberLiteral 42.0 )]
-      "42" |> JsEval.parse
+      let expected = NumberLiteral 42.0
+      "42" |> parseSingleExpression
       |> should (be.equalTo expected))
 
     it "Parses + operators" (fun _ ->
-      let expected = Program [ ExpressionStmt ( Plus ( NumberLiteral 2.0, NumberLiteral 1.0 ) ) ]
-      "2 + 1" |> JsEval.parse |>should (be.equalTo expected))
+      let expected = Plus ( NumberLiteral 2.0, NumberLiteral 1.0 ) 
+      "2 + 1" |> parseSingleExpression |>should (be.equalTo expected))
 
     it "Parses - operators" (fun _ ->
-      let expected = Program [ ExpressionStmt ( Minus ( NumberLiteral 2.0, NumberLiteral 1.0 ) ) ]
-      "2 - 1" |> JsEval.parse |>should (be.equalTo expected))
+      let expected = Minus ( NumberLiteral 2.0, NumberLiteral 1.0 ) 
+      "2 - 1" |> parseSingleExpression |>should (be.equalTo expected))
   ]
