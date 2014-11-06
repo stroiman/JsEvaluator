f00fa10986355b455a23673a7129f66dcba982cf
Peter Strøiman <peter@stroiman.com>
2014-11-06 08:44:47 +0100

    Changed indentation to 4 spaces
    Makes it easier to recognize blocks

--- JsEvaluator/Program.fs
+++ JsEvaluator/Program.fs
@@ -15,54 +15,54 @@ let parse input =
     Parser.start token lexbuf
 
 let eval (env:Environment) (program : Program) =
-  let rec evalExpr (env:Environment) = function
-    | NumberLiteral x -> JsNumber x
-    | Plus(x, y) ->
-      match (evalExpr env x, evalExpr env y) with
-      | JsNumber a, JsNumber b -> JsNumber (a + b)
-      | _ -> failwith "Can only subtract numbers"
-    | Minus(x, y) ->
-      match (evalExpr env x, evalExpr env y) with
-      | JsNumber a, JsNumber b -> JsNumber (a - b)
-      | _ -> failwith "Can only subtract numbers"
-    | LessThanOrEqual (x,y) ->
-      match evalExpr env x, evalExpr env y with
-      | JsNumber a, JsNumber b -> JsBool (a <= b)
-      | _ -> failwithf "Can only compare numbers"
-    | VariableLookup x -> env.Get x
-    | FunctionDefinition(args, body) ->
-        JsFunction(env,args, body)
-    | FunctionInvocation(f,ps) ->
-        let fObj = evalExpr env f
-        let parameters = ps |> List.map (evalExpr env)
-        match fObj with
-        | JsFunction (env,args,body) ->
-            let nestedEnv = env |> Environment.CreateChild
-            let add (a,p) : unit = nestedEnv.Add a p
-            let argValuePaies = List.zip args parameters
-            List.iter add argValuePaies
-            evalStmtList nestedEnv body
-        | x -> failwithf "Not a function: %A" x
-  and evalStmtList (env:Environment) =
-    let rec iter last = function
-      | [] -> last
-      | x::xs ->
-        match x with
-        | ReturnStmt y -> evalExpr env y
-        | If(c,b) ->
-          match evalExpr env c with
-          | JsBool(true) -> evalStmtList env (b@xs)
-          | JsBool(false) -> evalStmtList env xs
-          | x -> failwith "Not a boolean value: %A" x
-        | ExpressionStmt x -> iter (evalExpr env x) xs
-        | VariableDefinition (n,x) ->
-            let value = evalExpr env x
-            env.Add n value
-            iter last xs
-    iter JsUndefined
+    let rec evalExpr (env:Environment) = function
+      | NumberLiteral x -> JsNumber x
+      | Plus(x, y) ->
+          match (evalExpr env x, evalExpr env y) with
+          | JsNumber a, JsNumber b -> JsNumber (a + b)
+          | _ -> failwith "Can only subtract numbers"
+      | Minus(x, y) ->
+          match (evalExpr env x, evalExpr env y) with
+          | JsNumber a, JsNumber b -> JsNumber (a - b)
+          | _ -> failwith "Can only subtract numbers"
+      | LessThanOrEqual (x,y) ->
+          match evalExpr env x, evalExpr env y with
+          | JsNumber a, JsNumber b -> JsBool (a <= b)
+          | _ -> failwithf "Can only compare numbers"
+      | VariableLookup x -> env.Get x
+      | FunctionDefinition(args, body) ->
+          JsFunction(env,args, body)
+      | FunctionInvocation(f,ps) ->
+          let fObj = evalExpr env f
+          let parameters = ps |> List.map (evalExpr env)
+          match fObj with
+          | JsFunction (env,args,body) ->
+              let nestedEnv = env |> Environment.CreateChild
+              let add (a,p) : unit = nestedEnv.Add a p
+              let argValuePaies = List.zip args parameters
+              List.iter add argValuePaies
+              evalStmtList nestedEnv body
+          | x -> failwithf "Not a function: %A" x
+    and evalStmtList (env:Environment) =
+        let rec iter last = function
+          | [] -> last
+          | x::xs ->
+              match x with
+              | ReturnStmt y -> evalExpr env y
+              | If(c,b) ->
+                  match evalExpr env c with
+                  | JsBool(true) -> evalStmtList env (b@xs)
+                  | JsBool(false) -> evalStmtList env xs
+                  | x -> failwith "Not a boolean value: %A" x
+              | ExpressionStmt x -> iter (evalExpr env x) xs
+              | VariableDefinition (n,x) ->
+                  let value = evalExpr env x
+                  env.Add n value
+                  iter last xs
+        iter JsUndefined
 
-  match program with
-  | Program x -> evalStmtList env x
+    match program with
+    | Program x -> evalStmtList env x
 
 [<EntryPoint>]
 let main argv = 
